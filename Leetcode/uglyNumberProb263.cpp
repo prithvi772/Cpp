1ac2ecd0b964fd8af180f00db05f95c095bf17d2
Prithvi Nandkumar Gadekar <115449051+prithvi772@users.noreply.github.com>
2026-02-09 14:32:36 +0530

    Refactor ugly number check logic in main function

--- Leetcode/uglyNumberProb263.cpp
+++ Leetcode/uglyNumberProb263.cpp
@@ -1,30 +1,42 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class Solution {
-public:
-    bool isUgly(int n) {
-        if (n <= 0) return false;
-
-        while (n % 2 == 0) n /= 2;
-        while (n % 3 == 0) n /= 3;
-        while (n % 5 == 0) n /= 5;
-
-        return n == 1;
-    }
-};
-
 int main() {
-    Solution sol;
+    int n = 6;
 
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
+    if (n <= 0) {
+        cout << "Not Ugly";
+        return 0;
+    }
 
-    if (sol.isUgly(n))
-        cout << "Ugly Number" << endl;
+    for (int i = 2; i <= n; i++) {
+        if (n % i == 0) {
+            if (i != 2 && i != 3 && i != 5) {
+                cout << "Not Ugly";
+                return 0;
+            }
+
+            // remove all occurrences of valid factor
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
+    }
+
+    if (n == 1)
+        cout << "Ugly";
     else
-        cout << "Not an Ugly Number" << endl;
+        cout << "Not Ugly";
 
     return 0;
+
+    // bool isUgly(int n) {
+    //     if (n <= 0) return false;
+
+    //     while (n % 2 == 0) n /= 2;
+    //     while (n % 3 == 0) n /= 3;
+    //     while (n % 5 == 0) n /= 5;
+
+    //     return n == 1;
+    // }
 }
